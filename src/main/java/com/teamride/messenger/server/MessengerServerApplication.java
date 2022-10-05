dbf600c86441db7216ac3d345fc48a63b1d0ada5
Min Hong <shmin7777@naver.com>
2022-10-05 16:26:46 +0900

    init

---
+++ src/main/java/com/teamride/messenger/server/MessengerServerApplication.java
@@ -0,0 +1,13 @@
+package com.teamride.messenger.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamride/messenger/server/MessengerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamride.messenger.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
