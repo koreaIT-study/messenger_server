9dbe0c02a4dd35dd89de526aba4ac6722423c81b
Min Hong <shmin7777@naver.com>
2022-10-27 11:08:14 +0900

    shell script docker 실행하게 변경

--- shell/messenger_server.sh
+++ shell/messenger_server.sh
@@ -1,76 +1,20 @@
 #!/bin/sh
 
-#SERVER_HOME='/home/ec2-user/deploy/messenger_server'
-if [ "${SERVER_HOME:-}" = "" ];
-    then SERVER_HOME="$(dirname "$(cd "$(dirname "$0")"; "pwd")")";
-fi
-SERVER_LIBS="${SERVER_HOME}/libs"
-SERVER_CONF="${SERVER_HOME}/conf"
-SERVER_LOGS="${SERVER_HOME}/logs"
-echo "SERVER_HOME dir : $SERVER_HOME"
-
-JAR_FILE=`find $SERVER_HOME -name messenger_server-*.jar`
-echo "server JAR file : $JAR_FILE"
-
-print_usage() {
-  echo "messenger_server usage:"
-  echo "    ./messenger_server.sh [option arguments] [start|stop|restart]"
-  echo "    --help, -h, ?)"
-  echo "         : print this message"
-  echo "    --config, -c {config file location})"
-  echo "         : application.yml config file location"
-  echo "    --rest.port, -r {port number})"
-  echo "         : WAS port for execute REST API"
-  echo "    start|stop|restart) server start | stop | restart"
-  exit 0
-}
-
-MAX_WAIT_CNT=3
-CURRENT_WAIT_CNT=0
-wait_graceful_shutdown() {
-  echo "Process $1 health check..."
-  PID=`ps -ef | grep $1 | grep spring | grep -v grep | awk '{print $2}'`
-  if [[ -z $PID ]]; then
-    echo "Process $1 shutdown complete!"
-  else
-    echo "wait 5 seconds..."
-    sleep 5
-    if [[ $CURRENT_WAIT_CNT -lt $MAX_WAIT_CNT ]]; then
-      CURRENT_WAIT_CNT=$CURRENT_WAIT_CNT+1
-      wait_graceful_shutdown $PID
-    else
-      echo "failed graceful shutdown!!!"
-      echo "execute force kill process $PID"
-      kill -15 $PID
-    fi
-  fi
-}
-
 stop_server() {
-  PROC=`ps aux | grep messenger_server- | grep spring`
-  if [[ $PROC == *"messenger_server-"* ]]; then
+  PROC=`docker ps -aq  -f 'NAME=messenger-server'`
+  if [[ -n $PROC ]]; then
     echo "Process is running."
-    PID=`ps -ef | grep messenger_server- | grep spring | grep -v grep | awk '{print $2}'`
-    kill -9 $PID
-    wait_graceful_shutdown $PID
+    docker stop $PROC
+    docker rm $PROC
+    echo "Process stoped"
   else
     echo "Process is not running."
   fi
 }
 
-JVM_OPTS="-Xms4096m -Xmx4096m -server -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:+DisableExplicitGC \
-          -verbose:gc -XX:+PrintGCDetails -XX:-PrintGCTimeStamps -Xloggc:$SERVER_LOGS/messenger_server-gc.log \
-          -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$SERVER_LOGS -XX:+UseGCOverheadLimit"
-SERVER_OPTS="-DSERVER.home=$SERVER_HOME"
-APP_ARGS=""
-
 start_server() {
-  SERVER_OPTS="$SERVER_OPTS -Dspring.config.location=optional:file:$SERVER_CONF/"
-  echo "server options : $JVM_OPTS $SERVER_OPTS"
-  echo "application arguments : $APP_ARGS"
-
-  # execute server
-  nohup java -jar $JAR_FILE 1> /dev/null 2>&1 & 
+  echo "container start~~"
+  docker run --name messenger-server -d -p 12000:12000 shmin7777/messenger-server
 }
 
 # input argument check & execute
