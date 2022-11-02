38ee3541864901ce0fd201f8f4bcb017f97bfd40
dongJ <alalal2234@gmail.com>
2022-11-03 03:00:26 +0900

    221103 smtp 구현중

--- src/main/java/com/teamride/messenger/server/controller/AdminController.java
+++ src/main/java/com/teamride/messenger/server/controller/AdminController.java
@@ -1,10 +1,12 @@
 package com.teamride.messenger.server.controller;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.mail.MessagingException;
 
 import org.apache.ibatis.javassist.NotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.teamride.messenger.server.dto.AdminDTO;
@@ -18,22 +20,32 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 public class AdminController {
-	private final AdminService adminService;
+    private final AdminService adminService;
+    private final MailService mailService;
 
-	@PostMapping("/social_login")
-	public void socialLogin(@RequestBody AdminDTO adminDTO) {
-		log.info("server 들어옴");
-		adminService.checkAndInsertUser(adminDTO);
-	}
+    @PostMapping("/social_login")
+    public void socialLogin(@RequestBody AdminDTO adminDTO) {
+        log.info("server 들어옴");
+        adminService.checkAndInsertUser(adminDTO);
+    }
 
-	@PostMapping("/loginAction")
-	public RestResponse loginAction(HttpServletRequest req, @RequestBody AdminDTO adminDTO) {
-		try {
-			AdminDTO userInfo = adminService.getUserInfo(adminDTO);
-			return new RestResponse(userInfo);
-		} catch (NotFoundException e) {
-			return new RestResponse(1, e.getLocalizedMessage(), null);
-		}
-	}
+    @PostMapping("/loginAction")
+    public RestResponse loginAction(@RequestBody AdminDTO adminDTO) {
+        try {
+            final AdminDTO userInfo = adminService.getUserInfo(adminDTO);
+            return new RestResponse(userInfo);
+        } catch (NotFoundException e) {
+            return new RestResponse(1, e.getLocalizedMessage(), null);
+        }
+    }
 
+    @GetMapping("/smtpRequest")
+    public RestResponse smtp(@RequestParam String email) {
+        try {
+            return new RestResponse(mailService.joinEmail(email));
+        } catch (MessagingException e) {
+            log.error("mail send error", e.getLocalizedMessage());
+            return new RestResponse(1, e.getLocalizedMessage(), null);
+        }
+    }
 }
