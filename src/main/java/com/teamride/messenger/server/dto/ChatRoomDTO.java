723c6eca0f8009b0406280146b4b6a47ef0faf8d
dongJ <alalal2234@gmail.com>
2022-12-01 16:50:40 +0900

    chatDTO 오류 수정

--- src/main/java/com/teamride/messenger/server/dto/ChatRoomDTO.java
+++ src/main/java/com/teamride/messenger/server/dto/ChatRoomDTO.java
@@ -3,6 +3,8 @@ package com.teamride.messenger.server.dto;
 import java.util.List;
 import java.util.UUID;
 
+import org.apache.kafka.common.protocol.types.Field.Str;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
@@ -24,6 +26,8 @@ public class ChatRoomDTO {
     private String time;
     private int cnt;
     private String isGroup;
+    private List<String> userId;
+
 
     public static ChatRoomDTO create(String roomName, String isGroup){
         ChatRoomDTO room = new ChatRoomDTO();
