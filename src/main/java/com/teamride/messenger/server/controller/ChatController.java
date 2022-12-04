af31335b765dbb4ae4ca795cb628054b74fdec60
shmin7777 <shmin7777@naver.com>
2022-12-04 20:30:33 +0900

    message 전체 가져오는 code 정렬

--- src/main/java/com/teamride/messenger/server/controller/ChatController.java
+++ src/main/java/com/teamride/messenger/server/controller/ChatController.java
@@ -24,6 +24,8 @@ public class ChatController {
 	@GetMapping("/get-chat-message")
 	public List<ChatMessageDTO> getChatMessage(String roomId) {
 		List<ChatMessageDTO> result = chatService.getAllMessageWithRoomId(roomId);
+		result.sort((o1,o2)->o1.getTimestamp().compareTo(o2.getTimestamp()));
+		
 		log.info("get chat message api");
 		log.info("roomId is {}, result :: {} ", roomId, result);
 		return result;
