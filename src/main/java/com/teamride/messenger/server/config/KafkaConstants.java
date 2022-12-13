bd3792e6da27d7c2d2bc9832a2982fc224343de7
Min Hong <shmin7777@naver.com>
2022-12-13 16:19:24 +0900

    kafkaconstants 사용 - controller에서 하드코딩 되어있는거 정리

--- src/main/java/com/teamride/messenger/server/config/KafkaConstants.java
+++ src/main/java/com/teamride/messenger/server/config/KafkaConstants.java
@@ -3,6 +3,7 @@ package com.teamride.messenger.server.config;
 public class KafkaConstants {
 //  public static final String KAFKA_TOPIC = "kafka-chat";
     public static final String CHAT_SERVER = "chat-server";
+    public static final String CHAT_CLIENT = "chat-client";
   public static final String GROUP_ID = "chatting";
   public static final String KAFKA_BROKER = "35.216.1.250:9092";
 }
