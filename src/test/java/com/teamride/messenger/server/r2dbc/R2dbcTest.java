3ba0b3478a2c21bd14011befbbd213c9d2e30054
shmin7777 <shmin7777@naver.com>
2022-12-19 22:01:47 +0900

    test code 주석처리

--- src/test/java/com/teamride/messenger/server/r2dbc/R2dbcTest.java
+++ src/test/java/com/teamride/messenger/server/r2dbc/R2dbcTest.java
@@ -1,53 +1,53 @@
-package com.teamride.messenger.server.r2dbc;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import reactor.core.publisher.Flux;
-
-@Slf4j
-@SpringBootTest
-public class R2dbcTest {
-
-	@Autowired
-	UserRepository userRepository;
-
-	@Test
-	void userRepoTest() {
-		Flux<User> userFlux = userRepository.findAll().log();
-		
-		userFlux.collectList().block().forEach(user->{
-			log.info("user::"+user);
-		});
-	}
-
-}
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Table(name = "USER")
-class User {
-
-	@Id
-	private int id; // uuid
-
-	private String email;
-	private String name;
-	private String pwd;
-	private String profilePath; // 나중에
-	private String profileImg;
-	private String profileOriginalImg;
-}
-
-interface UserRepository extends ReactiveCrudRepository<User, Integer> {
-
-}
+//package com.teamride.messenger.server.r2dbc;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.relational.core.mapping.Table;
+//import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import reactor.core.publisher.Flux;
+//
+//@Slf4j
+//@SpringBootTest
+//public class R2dbcTest {
+//
+//	@Autowired
+//	UserRepository userRepository;
+//
+//	@Test
+//	void userRepoTest() {
+//		Flux<User> userFlux = userRepository.findAll().log();
+//		
+//		userFlux.collectList().block().forEach(user->{
+//			log.info("user::"+user);
+//		});
+//	}
+//
+//}
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Table(name = "USER")
+//class User {
+//
+//	@Id
+//	private int id; // uuid
+//
+//	private String email;
+//	private String name;
+//	private String pwd;
+//	private String profilePath; // 나중에
+//	private String profileImg;
+//	private String profileOriginalImg;
+//}
+//
+//interface UserRepository extends ReactiveCrudRepository<User, Integer> {
+//
+//}
