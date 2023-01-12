9689688b587372dad2562cb2e3da137afec155c3
ted <minjagot@naver.com>
2023-01-12 22:34:52 +0900

    파일 다운로드 작업 1

--- src/main/java/com/teamride/messenger/server/controller/ChatController.java
+++ src/main/java/com/teamride/messenger/server/controller/ChatController.java
@@ -1,10 +1,11 @@
 package com.teamride.messenger.server.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import com.teamride.messenger.server.config.KafkaConstants;
+import org.apache.commons.fileupload.disk.DiskFileItem;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.teamride.messenger.server.dto.ChatMessageDTO;
@@ -13,11 +14,12 @@ import com.teamride.messenger.server.service.ChatService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import javax.annotation.PostConstruct;
-import java.util.HashMap;
+import java.io.*;
+import java.nio.file.Files;
 import java.util.List;
 
 @Slf4j
@@ -62,5 +64,29 @@ public class ChatController {
 	@PostMapping("/messege-file")
 	public Mono<Integer> fileSend(@RequestPart(required = false, value = "files")List<MultipartFile> files, @RequestPart(value = "msg")ChatMessageDTO msg){
 		return chatService.fileSend(files, msg);
+	}
+
+	@GetMapping("/downFile/{roomId}/{msg}")
+	public MultiValueMap<String, Object> fileDownload(@PathVariable String roomId, @PathVariable String msg){
+		MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+		File file = new File(KafkaConstants.MSG_FILE_LOCATION + '/' + roomId + '/' + msg);
+		try {
+			DiskFileItem fileItem = new DiskFileItem(msg.substring(msg.lastIndexOf("||"))
+													, Files.probeContentType(file.toPath())
+													, false
+													, file.getName()
+													, (int) file.length()
+													, file.getParentFile());
+			FileInputStream input = new FileInputStream(file);
+			OutputStream os = fileItem.getOutputStream();
+			IOUtils.copy(input,os);
+
+			MultipartFile multipartFile = new CommonsMultipartFile(fileItem);
+			map.add("file", multipartFile);
+			return map;
+		} catch (IOException e) {
+			map.add("status", false);
+			return map;
+		}
 	}
 }
