d961d9f936c9a5d38c98e5090b09230d185f9a32
shmin7777 <shmin7777@naver.com>
2023-01-12 23:39:03 +0900

    file controller 수정

--- src/main/java/com/teamride/messenger/server/controller/ChatController.java
+++ src/main/java/com/teamride/messenger/server/controller/ChatController.java
@@ -1,26 +1,29 @@
 package com.teamride.messenger.server.controller;
 
-import com.teamride.messenger.server.config.KafkaConstants;
-import org.apache.commons.fileupload.disk.DiskFileItem;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.springframework.util.LinkedMultiValueMap;
+import java.io.File;
+import java.util.List;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.teamride.messenger.server.config.KafkaConstants;
 import com.teamride.messenger.server.dto.ChatMessageDTO;
 import com.teamride.messenger.server.dto.ChatRoomDTO;
 import com.teamride.messenger.server.service.ChatService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.io.*;
-import java.nio.file.Files;
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -62,30 +65,34 @@ public class ChatController {
 	}
 
 	@PostMapping("/messege-file")
-	public Mono<Integer> fileSend(@RequestPart(required = false, value = "files")List<MultipartFile> files, @RequestPart(value = "msg")ChatMessageDTO msg){
+	public Mono<Integer> fileSend(@RequestPart(required = false, value = "files") List<MultipartFile> files,
+			@RequestPart(value = "msg") ChatMessageDTO msg) {
 		return chatService.fileSend(files, msg);
 	}
 
 	@GetMapping("/downFile/{roomId}/{msg}")
-	public MultiValueMap<String, Object> fileDownload(@PathVariable String roomId, @PathVariable String msg){
-		MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+	public MultiValueMap<String, HttpEntity<?>> fileDownload(@PathVariable String roomId, @PathVariable String msg) {
+//		MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
 		File file = new File(KafkaConstants.MSG_FILE_LOCATION + '/' + roomId + '/' + msg);
-		try {
-			DiskFileItem fileItem = new DiskFileItem(msg.substring(msg.lastIndexOf("||"))
-													, Files.probeContentType(file.toPath())
-													, false
-													, file.getName()
-													, (int) file.length()
-													, file.getParentFile());
-			FileInputStream input = new FileInputStream(file);
-			OutputStream os = fileItem.getOutputStream();
-			IOUtils.copy(input,os);
-
-			MultipartFile multipartFile = new CommonsMultipartFile(fileItem);
-			map.add("file", multipartFile);
-			return map;
-		} catch (IOException e) {
-			return map;
-		}
+//		try {
+//			DiskFileItem fileItem = new DiskFileItem(msg.substring(msg.lastIndexOf("||"))
+//													, Files.probeContentType(file.toPath())
+//													, false
+//													, file.getName()
+//													, (int) file.length()
+//													, file.getParentFile());
+//			FileInputStream input = new FileInputStream(file);
+//			OutputStream os = fileItem.getOutputStream();
+//			IOUtils.copy(input,os);
+//
+//			MultipartFile multipartFile = new CommonsMultipartFile(fileItem);
+//			map.add("file", multipartFile);
+//			return map;
+//		} catch (IOException e) {
+//			return map;
+//		}
+		MultipartBodyBuilder builder = new MultipartBodyBuilder();
+		builder.part("file", new FileSystemResource(file));
+		return builder.build();
 	}
 }
